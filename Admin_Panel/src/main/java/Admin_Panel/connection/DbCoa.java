ee6fe31277a3060bed690a26847128f748152a98
Chanuka_Dilshan <140596566+ChanukaDilshanAmarathunga@users.noreply.github.com>
2024-04-26 11:30:09 +0530

    Created the Products adding functionality to the system , connected with the front end as well

--- Admin_Panel/src/main/java/Admin_Panel/connection/DbCoa.java
+++ Admin_Panel/src/main/java/Admin_Panel/connection/DbCoa.java
@@ -10,7 +10,7 @@ public class DbCoa {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		if (connection == null) {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/admin_panel", "root", "12345678");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/admin_panel", "root", "chanuka1111");
 			System.out.print("connected");
 
 		}
